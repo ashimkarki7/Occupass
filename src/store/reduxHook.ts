9ebc07df4a54ecb914503b646035b0b0067110fd
ashimkarki7 <35464231+ashimkarki7@users.noreply.github.com>
2025-06-27 16:41:29 +0545

    netlify configuration added redirect for client side route handling

--- src/store/reduxHook.ts
+++ src/store/reduxHook.ts
@@ -3,7 +3,7 @@ import {
   useDispatch,
   useSelector,
 } from 'react-redux';
-import type { RootState, AppDispatch } from 'src/store/store.ts';
+import type { RootState, AppDispatch } from './store.ts';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
