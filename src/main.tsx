680a07b801f0a6a603b12bff711dcf763d71f667
ashimkarki7 <35464231+ashimkarki7@users.noreply.github.com>
2025-06-27 16:20:51 +0545

    Initialized redux store and added wrapper to main component, Included husky for pre commit task runs

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
 import {ErrorBoundary} from '@/hoc/ErrorBoundary.tsx';
+import {Provider} from 'react-redux';
+import { store } from './store/store.ts';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <ErrorBoundary>
       <Router>
+          <Provider store={store}>
     <App />
+          </Provider>
       </Router>
       </ErrorBoundary>
   </StrictMode>,
