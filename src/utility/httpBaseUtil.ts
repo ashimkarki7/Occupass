fe0f51375c3dcabec3d098886ebca7a75915ca24
ashimkarki7 <35464231+ashimkarki7@users.noreply.github.com>
2025-06-29 17:15:22 +0545

    Feat: added Api fetching Helpers

---
+++ src/utility/httpBaseUtil.ts
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+export const httpBase = () => {
+  const baseURL3001 = import.meta.env.VITE_APP_URL;
+  const V2Headers = {
+    Accept: '*',
+    'Content-Type': 'text/plain',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers':
+      'Origin, X-Requested-With, Content-Type, Accept',
+  };
+
+  const api = axios.create({
+    baseURL: `${baseURL3001}`,
+    headers: { ...V2Headers },
+    responseType: 'json',
+  });
+
+  api.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      if (401 === error.response.status) {
+        console.log(error, '401');
+      }
+      if (404 === error.response.status) {
+        console.log(error, '404');
+      }
+      if (500 === error.response.status) {
+        console.log(error, '500');
+      }
+      return Promise.reject(JSON.stringify(error));
+    }
+  );
+
+  return api;
+};
